4cb014813c8c617e37d83622a543e1ea874c0270
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-08-10 20:51:55 +0700

    feat: swipable toast but only to the right

--- film_store/templates/global.js
+++ film_store/templates/global.js
@@ -45,4 +45,70 @@ if (toastCloseButton) {
       }, 5000);
     }, 50);
   }
+
+
+  // Draggable
+  const draggable = toastCloseButton.parentElement;
+  let isDragging = false;
+  let initialX;
+  let initialY;
+  let currentX;
+  let currentY;
+
+  draggable.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    initialX = e.clientX;
+    initialY = e.clientY;
+    toastCloseButton.parentElement.style = ``;
+  });
+  draggable.addEventListener('touchstart', (e) => {
+    isDragging = true;
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+    toastCloseButton.parentElement.style = ``;
+  });
+
+  draggable.addEventListener('mouseup', () => {
+    isDragging = false;
+    if(currentX > 30) {
+      toastCloseButton.parentElement.style = `
+        transform: translateX(100%);
+        transition: transform 0.25s ease-out;
+      `;
+    }
+    else {
+      draggable.style.transform = `translateX(${0}px) translateY(${0}px)`;
+    }
+  });
+
+  draggable.addEventListener('touchend', () => {
+    isDragging = false;
+    if(currentX > 30) {
+      toastCloseButton.parentElement.style = `
+        transform: translateX(100%);
+        transition: transform 0.25s ease-out;
+      `;
+    }
+    else {
+      draggable.style.transform = `translateX(${0}px) translateY(${0}px)`;
+    }
+  });
+
+  draggable.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+      e.preventDefault();
+      currentX = e.clientX - initialX;
+      currentY = e.clientY - initialY;
+      draggable.style.transform = `translateX(${currentX}px) translateY(${0}px)`;
+    }
+  });
+
+  draggable.addEventListener('touchmove', (e) => {
+    if (isDragging) {
+      e.preventDefault();
+      currentX = e.touches[0].clientX - initialX;
+      currentY = e.touches[0].clientY - initialY;
+      draggable.style.transform = `translateX(${currentX}px) translateY(${0}px)`;
+    }
+  });
 }
