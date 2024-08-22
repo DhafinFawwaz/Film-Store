e472bc57cc4611df52d5c18794ef3f5be6ac1965
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-08-22 19:55:29 +0700

    fix: misconfig test

--- film_store/app/test/test_misconfig.py
+++ film_store/app/test/test_misconfig.py
@@ -12,6 +12,8 @@ class TestMisconfig(EndToEndTest):
         def handle_response(response):
             if response.status == 404:
                 self.assertEqual(response.status, 404, f"Status Code {response.status} received for /")
+            elif response.status == 403:
+                self.assertEqual(response.status, 403, f"Status Code {response.status} received for /")
             else:
                 locator_404 = page.locator('h1:text("404")')
                 self.assertTrue(locator_404.is_visible(), "404 page should be displayed for non-existent page")
