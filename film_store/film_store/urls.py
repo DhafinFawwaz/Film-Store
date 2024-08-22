ef85dc4303d240c47aeb9cdaddc6e96a3d94085b
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-08-22 20:28:31 +0700

    nit: undo remove hot reload

--- film_store/film_store/urls.py
+++ film_store/film_store/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
     path('', include('app.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 
